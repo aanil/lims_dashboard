7f467a39b48f579ec28e65b4602ca13ee5fe90b7
Denis Moreno <denis.moreno@scilifelab.se>
2016-10-14 17:27:35 +0200

    rename

--- lims_dashboard/utils.py
+++ lims_dashboard/utils.py
@@ -8,7 +8,7 @@ def run_script(app, name, options):
     conf_obj=app.config['my_scripts'][name]
     command=[':']
     if conf_obj['type']=='python':
-        command=[app.config['python_exec'], os.path.join(app.config['SCRIPT_FOLDER'],app.config['my_scripts'][name]['command'])]
+        command=[app.config['python_exec'], os.path.join(app.config['SCRIPT_FOLDER'],app.config['my_scripts'][name]['script'])]
     command.extend(options.split())
 
     handle=subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
