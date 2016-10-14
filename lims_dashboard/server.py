db1362026a29c1071ca1c61288aa947c0883da38
Denis Moreno <denis.moreno@scilifelab.se>
2016-10-14 17:22:26 +0200

    allow python selection

--- lims_dashboard/server.py
+++ lims_dashboard/server.py
@@ -7,10 +7,11 @@ from flask import Flask, Blueprint
 from lims_dashboard.routes import my_bp
 
 
-def create_app(root_path=os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]):
+def create_app(root_path=os.path.split(os.path.split(os.path.realpath(__file__))[0])[0], python='python'):
     app = Flask(__name__, root_path=root_path)
     UPLOAD_FOLDER = os.path.join(root_path, 'uploads')
     SCRIPT_FOLDER = os.path.join(root_path, 'scripts')
+    app.config['python_exec']=python
     app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
     app.config['SCRIPT_FOLDER']=SCRIPT_FOLDER
     app.config['my_scripts']={}
--- lims_dashboard/utils.py
+++ lims_dashboard/utils.py
@@ -5,7 +5,10 @@ import subprocess
 def run_script(app, name, options):
     cwd=os.getcwd()
     os.chdir('{0}/uploads'.format(app.root_path))
-    command = [os.path.join(app.config['SCRIPT_FOLDER'],app.config['my_scripts'][name]['command'])]
+    conf_obj=app.config['my_scripts'][name]
+    command=['']
+    if conf_obj['type']='python':
+        command=[app.config['python_exec'], os.path.join(app.config['SCRIPT_FOLDER'],app.config['my_scripts'][name]['command'])]
     command.extend(options.split())
 
     handle=subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
