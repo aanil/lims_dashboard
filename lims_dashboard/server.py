387da5e692ea1c284ef060253f2155e4aee63dc0
Denis Moreno <denis.moreno@scilifelab.se>
2016-10-14 13:51:08 +0200

    wrapping app creation

--- lims_dashboard/server.py
+++ lims_dashboard/server.py
@@ -7,19 +7,22 @@ import yaml
 
 from flask import Flask, request, render_template, url_for
 
-ROOT_PATH = os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]
-print ROOT_PATH
-app = Flask(__name__, template_folder='templates', static_folder='static', root_path=ROOT_PATH)
+app=None
 
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
 
-app.config['my_scripts']={}
-for conf_file in glob.glob(os.path.join(app.root_path, 'conf', '*.conf')):
-    with open(conf_file) as sconf:
-        app.config['my_scripts'].update(yaml.load(sconf))
+def create_app(root_path=os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]):
+    global app
+    app = Flask(__name__, template_folder='templates', static_folder='static', root_path=root_path)
+    UPLOAD_FOLDER = 'uploads'
+    app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+    app.config['my_scripts']={}
+    for conf_file in glob.glob(os.path.join(app.root_path, 'conf', '*.conf')):
+        with open(conf_file) as sconf:
+            app.config['my_scripts'].update(yaml.load(sconf))
+    return app
 
-app.debug=True
+if not app:
+    app=create_app()
 
 @app.route('/', methods=['GET'])
 def display_dashboard():
