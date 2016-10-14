b6db6f3864df69ed668349451670d7fd8fcf8018
Denis Moreno <denis.moreno@scilifelab.se>
2016-10-14 17:26:35 +0200

    typo

--- lims_dashboard/utils.py
+++ lims_dashboard/utils.py
@@ -6,8 +6,8 @@ def run_script(app, name, options):
     cwd=os.getcwd()
     os.chdir('{0}/uploads'.format(app.root_path))
     conf_obj=app.config['my_scripts'][name]
-    command=['']
-    if conf_obj['type']='python':
+    command=[':']
+    if conf_obj['type']=='python':
         command=[app.config['python_exec'], os.path.join(app.config['SCRIPT_FOLDER'],app.config['my_scripts'][name]['command'])]
     command.extend(options.split())
 
